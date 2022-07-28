a192d5c9111b51957abd3af3998fe8a96e566a5e
JeffreyGilDeimos <jeffroxgil@gmail.com>
2022-07-28 15:47:35 +0800

    Enable special buttons

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ let firstNumber;
 let secondNumber;
 let operation;
 let answer;
+let flag=0;
 
 console.log(firstNumber, operation,secondNumber)
-function clickNumber(input) {    
+
+function clickNumber(input) { 
+      
     if (operation===undefined) {
         if (firstNumber===undefined) {        
             firstNumber=input;
+            } else if (firstNumber===0 || flag===1){
+                firstNumber=input;
+                flag=0;
             } else  {
             firstNumber=firstNumber + input;
             }
@@ -23,44 +29,62 @@ function clickNumber(input) {
 }
 function clickOperation(input) {  
    operation=input;
+   if (firstNumber===undefined){
+        alert("No number to execute.");
+        operation=undefined;  
+   } 
    console.log(firstNumber, operation, secondNumber);
 }
 function clickEqual(){
-    switch(operation) {
-        case '+': {     
-        answer=Number(firstNumber) + Number(secondNumber);
-        console.log("= " + answer);
-        firstNumber=answer;
-        secondNumber=undefined;
-        operation=undefined;
-        break;
-        }
-        case '-': {     
-            answer=Number(firstNumber) - Number(secondNumber);
-            console.log("= " + answer);
-            firstNumber=answer;
-            secondNumber=undefined;
-            operation=undefined;
-            break;
-            }
-        case '*': {     
-            answer=Number(firstNumber) * Number(secondNumber);
-            console.log("= " + answer);
-            firstNumber=answer;
-            secondNumber=undefined;
-            operation=undefined;
-            break;
+    if (firstNumber===undefined) {        
+        alert("No number to execute.");
+        console.log(firstNumber, operation, secondNumber);
+    } else if (operation===undefined) {     
+        console.log(firstNumber, operation, secondNumber);
+    } else if (secondNumber===undefined) {        
+        alert("Please input the second number.");
+        console.log(firstNumber, operation, secondNumber);
+    } else {       
+        switch(operation) {
+            case '+': {                             
+                answer=Number(firstNumber) + Number(secondNumber);
+                console.log("= " + answer);  
+                firstNumber=answer;
+                secondNumber=undefined;   
+                operation=undefined;    
+                flag=1;                 
+                break;
+            }          
+            case '-': {     
+                answer=Number(firstNumber) - Number(secondNumber);   
+                console.log("= " + answer);               
+                firstNumber=answer;
+                secondNumber=undefined;   
+                operation=undefined;  
+                flag=1;                 
+                break;
+            }                
+            case '*': {     
+                answer=Number(firstNumber) * Number(secondNumber); 
+                console.log("= " + answer);               
+                firstNumber=answer;
+                secondNumber=undefined;
+                operation=undefined;
+                flag=1; 
+                break;
             }    
-        case '/': {     
-            answer=Number(firstNumber) / Number(secondNumber);
-            console.log("= " + answer);
-            firstNumber=answer;
-            secondNumber=undefined;
-            operation=undefined;
-            break;
+            case '/': {     
+                answer=Number(firstNumber) / Number(secondNumber);
+                console.log("= " + answer);    
+                firstNumber=answer;
+                secondNumber=undefined;   
+                operation=undefined; 
+                flag=1;                     
+                break;
+            }            
+            default: {
+                    alert("Unknown operation.");
             }
-        default: {
-            console.log("Operation unknown");
         }
     }
 }
@@ -69,4 +93,17 @@ function allClear(){
     secondNumber=undefined;
     operation=undefined;    
     console.log(firstNumber, operation, secondNumber);
+}
+function clickSign() {
+    if (firstNumber===undefined) {
+        alert("No number to execute.");
+    } else if (operation && secondNumber===undefined) {
+        alert("No second number to execute.");
+    } else if (secondNumber) {
+        secondNumber = secondNumber * -1;
+        console.log(firstNumber, operation, secondNumber);    
+    } else {
+        firstNumber = firstNumber * -1;
+        console.log(firstNumber, operation, secondNumber);
+    }
 }
